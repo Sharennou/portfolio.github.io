794ff432f050a2e2278ead646535c14cbc0ad8f7
Sharenn <106980488+Sharennou@users.noreply.github.com>
2025-01-23 19:41:59 +0100

    Delete sae105/SAE 105 directory

--- sae105/SAE 105/JS/nom-utilisateur1.js
+++
@@ -1,7 +0,0 @@
-function redirectToHome() {
-    const username = document.getElementById('username').value;
-
-    localStorage.setItem('username', username);
-
-    window.location.href = '/SAE 105/HTML/index.html';
-}
